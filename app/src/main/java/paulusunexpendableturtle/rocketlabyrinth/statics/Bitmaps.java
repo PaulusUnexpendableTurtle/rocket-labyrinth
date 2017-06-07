343ca916a3bf116e6656cff3c15325ecbf043ba2
Paul Sokolov Jr <sokolov.p64@gmail.com>
2017-06-07 13:36:16 +0300

    1.2.5 - Simplified bitmap decoding; added canvas button for game thread.

--- app/src/main/java/paulusunexpendableturtle/rocketlabyrinth/statics/Bitmaps.java
+++ app/src/main/java/paulusunexpendableturtle/rocketlabyrinth/statics/Bitmaps.java
@@ -3,21 +3,32 @@ package paulusunexpendableturtle.rocketlabyrinth.statics;
 import android.content.res.Resources;
 
 import paulusunexpendableturtle.rocketlabyrinth.R;
-import paulusunexpendableturtle.rocketlabyrinth.gui.Hero;
-import paulusunexpendableturtle.rocketlabyrinth.gui.Sprite;
+import paulusunexpendableturtle.rocketlabyrinth.gui.sprites.CanvasButton;
+import paulusunexpendableturtle.rocketlabyrinth.gui.sprites.Hero;
+import paulusunexpendableturtle.rocketlabyrinth.gui.sprites.Sprite;
 
 import static paulusunexpendableturtle.rocketlabyrinth.statics.IconicConstants.maxlen;
 
 public final class Bitmaps {
     public static Sprite heart, cell, back, portal, pause;
     public static Hero hero;
+    public static CanvasButton exit;
 
-    public static synchronized void decodeResources(Resources c){
-        heart = new Sprite(c, R.drawable.fuel1, c.getDisplayMetrics().widthPixels + maxlen, c.getDisplayMetrics().heightPixels + maxlen);
-        cell = new Sprite(c, R.drawable.cell, c.getDisplayMetrics().widthPixels + maxlen, c.getDisplayMetrics().heightPixels + maxlen);
-        back = new Sprite(c, R.drawable.background_game, c.getDisplayMetrics().widthPixels + maxlen, c.getDisplayMetrics().heightPixels + maxlen);
-        portal = new Sprite(c, R.drawable.portal, c.getDisplayMetrics().widthPixels + maxlen, c.getDisplayMetrics().heightPixels + maxlen);
-        pause = new Sprite(c,R.drawable.pause_button_1, c.getDisplayMetrics().widthPixels + maxlen, c.getDisplayMetrics().heightPixels + maxlen);
+    public static synchronized void tryDecodeResources(Resources c){
+
+        if(back != null) return;
+
+        int w = c.getDisplayMetrics().widthPixels,
+                h = c.getDisplayMetrics().heightPixels;
+
+        heart = new Sprite(c, R.drawable.fuel1, w, h);
+        cell = new Sprite(c, R.drawable.cell, w, h);
+        back = new Sprite(c, R.drawable.background_game, w + maxlen, h + maxlen);
+        portal = new Sprite(c, R.drawable.portal, w, h);
+        pause = new Sprite(c,R.drawable.pause_button_1, w, h);
+
+        int[] color = {255, 255, 255, 255};
+        exit = new CanvasButton(c, R.drawable.overlay_button_shape, w, h, c.getString(R.string.quit_game_process_button), color);
 
         int[] t = {R.drawable.herow,
                 R.drawable.heroa,
@@ -26,7 +37,4 @@ public final class Bitmaps {
         hero = new Hero(c, t);
     }
 
-    public static synchronized boolean isNull(){
-        return back == null;
-    }
 }
