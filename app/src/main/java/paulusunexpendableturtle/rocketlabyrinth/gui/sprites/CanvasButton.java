82869758917ac94c314a732d5e300de3a0ce76b3
Paul Sokolov Jr <sokolov.p64@gmail.com>
2017-07-02 12:46:21 +0300

    1.3 - Good design for exit button.

--- app/src/main/java/paulusunexpendableturtle/rocketlabyrinth/gui/sprites/CanvasButton.java
+++ app/src/main/java/paulusunexpendableturtle/rocketlabyrinth/gui/sprites/CanvasButton.java
@@ -22,9 +22,17 @@ public class CanvasButton extends Sprite {
 
         Paint p = new Paint();
         p.setTextAlign(Paint.Align.CENTER);
+
+        p.setStyle(Paint.Style.STROKE);
         p.setTextSize(getHeight(canvas) >> 1);
+        p.setStrokeJoin(Paint.Join.ROUND);
+        p.setStrokeWidth(getHeight(canvas) / 20);
+        p.setColor(Color.BLACK);
+        canvas.drawText(text, x + getWidth(canvas) / 2, y + 3 * getHeight(canvas) / 4, p);
+
+        p.setStyle(Paint.Style.FILL);
         p.setColor(Color.argb(textColor[0], textColor[1], textColor[2], textColor[3]));
-        canvas.drawText(text, x + (getWidth(canvas) >> 1), y + (getHeight(canvas) >> 1), p);
+        canvas.drawText(text, x + getWidth(canvas) / 2, y + 3 * getHeight(canvas) / 4, p);
     }
 
 }
