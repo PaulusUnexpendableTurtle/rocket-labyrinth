f27122ffb385fbc0e4a62d82e136328fdf21649e
Paul Sokolov Jr <sokolov.p64@gmail.com>
2017-07-02 16:07:56 +0300

    1.3 - Well, name tells it all. [UNFINISHED]

---
+++ app/src/main/java/paulusunexpendableturtle/rocketlabyrinth/input/SaveWriter.java
@@ -0,0 +1,20 @@
+package paulusunexpendableturtle.rocketlabyrinth.input;
+
+import android.content.res.Resources;
+
+import paulusunexpendableturtle.rocketlabyrinth.game.Level;
+import paulusunexpendableturtle.rocketlabyrinth.game.modes.Mode;
+
+public class SaveWriter {
+
+    private Resources res;
+
+    public SaveWriter(Resources res){
+        this.res = res;
+    }
+
+    public void write(Mode mode, Level level){
+        //TODO: do it
+    }
+
+}
