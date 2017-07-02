8e85cad7a1a616f94f8b7d0084dd6a7f64733156
Paul Sokolov Jr <sokolov.p64@gmail.com>
2017-07-02 16:07:41 +0300

    1.3 - Well, name tells it all. [UNFINISHED]

---
+++ app/src/main/java/paulusunexpendableturtle/rocketlabyrinth/input/SaveReader.java
@@ -0,0 +1,32 @@
+package paulusunexpendableturtle.rocketlabyrinth.input;
+
+import android.content.res.Resources;
+
+import paulusunexpendableturtle.rocketlabyrinth.game.Level;
+import paulusunexpendableturtle.rocketlabyrinth.game.modes.Mode;
+
+//import static paulusunexpendableturtle.rocketlabyrinth.R.raw.save;
+
+public class SaveReader {
+
+    //private Resources res;
+
+    public SaveReader(Resources res){
+        //this.res = res;
+    }
+
+    public Mode readMode(){
+
+        //TODO: do it
+        return null;
+
+    }
+
+    public Level readLevel(){
+
+        //TODO: do it
+        return null;
+
+    }
+
+}
